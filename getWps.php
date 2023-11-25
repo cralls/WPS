f0faec25044291ba5c47994fe8d88d7cb1d1c21d
Casey Ralls <cralls@vectorns.com>
2023-11-24 19:46:14 -0700

    Update getWps.php

--- getWps.php
+++ getWps.php
@@ -117,11 +117,10 @@ function getItems($brand, $apiToken, $objectManager) {
                 $existingProduct = $productRepository->get($item['sku']);
                 
                 // Check if wps_item_id is set, if not, update it
-                if ($existingProduct->getWpsItemId() == '') {
-                    echo ("Working on ".$item['sku']."\r\n");
+                if ($existingProduct->getWpsItemId() == '' && $existingProduct->getAccountingCategory() == '6392') {
+                    echo ( "[".date('Y-m-d H:i:s')."] Setting wps_item_id for ".$existingProduct->getSku()." to ".$item['id']."\r\n");
                     $existingProduct->setWpsItemId($item['id']);
                     $productRepository->save($existingProduct);
-                    echo ( "Set wps_item_id for ".$existingProduct->getSku()." to ".$existingProduct->getWpsItemId()."\r\n\r\n");
                 }
                 
                 // If the product exists, skip to the next item
@@ -336,7 +335,7 @@ function getItems($brand, $apiToken, $objectManager) {
             }
             
             // Stop the script after creating the first product
-            echo date('Y-m-d H:i:s')." - Product created with SKU ".$item['sku']."\r\n\r\n";
+            echo date('Y-m-d H:i:s')." - Product created with SKU ".$item['sku']."\r\n";
             error_log(date('Y-m-d H:i:s')." - Product created with SKU ".$item['sku']."\r\n", 3, '/home/'.get_current_user().'/public_html/var/log/wps.log');
             
             sleep(1); // Give the API a break
@@ -402,9 +401,9 @@ if (in_array('getItems', $argv)) {
     $processBrands = false;
     
     foreach($allBrands as $brand) {
-        //if ($brand['name'] === 'ALL BALLS') {
+        if ($brand['name'] === 'FLY RACING') {
             $processBrands = true;
-        //}
+        }
         
         if (!$processBrands) {
             continue;
